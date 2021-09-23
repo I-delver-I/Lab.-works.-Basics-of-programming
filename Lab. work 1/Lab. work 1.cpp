90ad8084f5126a0b25c789aa2eb9794026cce12f
Scarmaing-Whebrolted <dima.plugaturevchik@gmail.com>
2021-09-23 11:34:34 +0300

    Добавьте файлы проекта.

---
+++ Lab. work 1/Lab. work 1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+// Завдання: дані гіпотенуза і катет прямого трикутника.Знайти другий катет цього трикутника.
+
+int main()
+{
+	float Cathetus1, Cathetus2, Hypotenuse; // катети та гіпотенуза
+	cout << "Please, enter a length of cathetus: ";
+	cin >> Cathetus1; // введення довжини першого катета
+
+	if (Cathetus1 <= 0)
+	{
+		cout << "A side of an triangle musn't be less or equal to 0" << endl;
+		return 0;
+	}
+
+	cout << "Please, enter a length of hypotenuse: ";
+	cin >> Hypotenuse; // введення довжини гіпотенузи
+	if (Hypotenuse <= 0)
+	{
+		cout << "A side of an triangle musn't be less or equal to 0" << endl;
+		return 0;
+	}
+
+	Cathetus2 = sqrt(pow(Hypotenuse, 2) - pow(Cathetus1, 2)); // розрахунок другого катета за теоремою Піфагора
+	cout << "The second cathetus is equal to: " << Cathetus2; // виведення результату
+
+	return 0;
+}
