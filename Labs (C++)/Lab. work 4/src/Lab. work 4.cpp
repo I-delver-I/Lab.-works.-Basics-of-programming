ae7d44f2f25bda2e81b75aeb78f989021540b60e
Dmytro <dima.plugaturevchik@gmail.com>
2021-11-10 22:22:46 +0200

    Update

--- Labs (C++)/Lab. work 4/src/Lab. work 4.cpp
+++ Labs (C++)/Lab. work 4/src/Lab. work 4.cpp
@@ -1,26 +1,25 @@
 ﻿#include <iostream>
 #include <math.h>
-using namespace std;
 
 // TASK 25
 
 int main()
 {
-	unsigned long long n;
-	cout << "Enter a number which is equal to 4 or is bigger than the last one: ";
-	cin >> n;	// Initialization of n
+	long long n;
+	std::cout << "Enter a number which is equal to 4 or is bigger than the last one: ";
+	std::cin >> n;	// Initialization of n
 	while (n < 4)	// Checking of n for performing of the condition
 	{
-		cout << "Something went wrong. Enter the number again: ";
-		cin >> n;
+		std::cout << "Something went wrong. Enter the number again: ";
+		std::cin >> n;
 	}
 
 	double v3 = 0, v2 = 0, v1 = 1.5, v; // Initialization of three primary elements of the sequence
 	double &vn = v;
 
-	for (unsigned long long i = 4; i <= n; i++)	// Calculating of vn
+	for (long long i = 4; i <= n; i++)	// Calculating of vn
 	{
-		v = static_cast<double>(i + 1.) / static_cast<double>(pow(static_cast<double>(i), 2) + 1.) * v1 - v2 * v3;
+		v = (i + 1) / (pow(static_cast<double>(i), 2) + 1.) * v1 - v2 * v3;
 		v3 = v2;
 		v2 = v1;
 		v1 = v;
@@ -28,10 +27,10 @@ int main()
 	
 	if (vn != 0)	// Checking for belonging to the range of data type
 	{
-		cout << "vn is equal to " << vn;
+		std::cout << "vn is equal to " << vn;
 	}
 	else
 	{
-		cout << "Too big value";
+		std::cout << "Too big value";
 	}
 }
